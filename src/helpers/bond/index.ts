931e2b27aa3639869d62ea1e9c7ebfadce92cbbc
appleseed-iii <appleseed-iii@protonmail.com>
2021-12-02 06:56:42 -0600

    starting to update bond addresses

--- src/helpers/bond/index.ts
+++ src/helpers/bond/index.ts
@@ -18,28 +18,28 @@ export const mim = new StableBond({
     reserveContractAbi: StableReserveContract,
     networkAddrs: {
         [Networks.AVAX]: {
-            bondAddress: "0x694738E0A438d90487b4a549b201142c1a97B556",
+            bondAddress: "0x45dBD709B11C9bA26A2bb2942F6b033c6e218AfA",
             reserveAddress: "0x130966628846BFd36ff31a822705796e8cb8C18D",
         },
     },
     tokensInStrategy: "60500000000000000000000000",
 });
 
-export const wavax = new CustomBond({
-    name: "wavax",
-    displayName: "wAVAX",
-    bondToken: "AVAX",
-    bondIconSvg: AvaxIcon,
-    bondContractABI: WavaxBondContract,
-    reserveContractAbi: StableReserveContract,
-    networkAddrs: {
-        [Networks.AVAX]: {
-            bondAddress: "0xE02B1AA2c4BE73093BE79d763fdFFC0E3cf67318",
-            reserveAddress: "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7",
-        },
-    },
-    tokensInStrategy: "756916000000000000000000",
-});
+// export const wavax = new CustomBond({
+//     name: "wavax",
+//     displayName: "wAVAX",
+//     bondToken: "AVAX",
+//     bondIconSvg: AvaxIcon,
+//     bondContractABI: WavaxBondContract,
+//     reserveContractAbi: StableReserveContract,
+//     networkAddrs: {
+//         [Networks.AVAX]: {
+//             bondAddress: "0xE02B1AA2c4BE73093BE79d763fdFFC0E3cf67318",
+//             reserveAddress: "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7",
+//         },
+//     },
+//     tokensInStrategy: "756916000000000000000000",
+// });
 
 export const mimTime = new LPBond({
     name: "mim_time_lp",
@@ -57,20 +57,20 @@ export const mimTime = new LPBond({
     lpUrl: "https://www.traderjoexyz.com/#/pool/0x130966628846BFd36ff31a822705796e8cb8C18D/0xb54f16fB19478766A268F172C9480f8da1a7c9C3",
 });
 
-export const avaxTime = new CustomLPBond({
-    name: "avax_time_lp",
-    displayName: "TIME-AVAX LP",
-    bondToken: "AVAX",
-    bondIconSvg: AvaxTimeIcon,
-    bondContractABI: LpBondContract,
-    reserveContractAbi: LpReserveContract,
-    networkAddrs: {
-        [Networks.AVAX]: {
-            bondAddress: "0xc26850686ce755FFb8690EA156E5A6cf03DcBDE1",
-            reserveAddress: "0xf64e1c5B6E17031f5504481Ac8145F4c3eab4917",
-        },
-    },
-    lpUrl: "https://www.traderjoexyz.com/#/pool/AVAX/0xb54f16fB19478766A268F172C9480f8da1a7c9C3",
-});
+// export const avaxTime = new CustomLPBond({
+//     name: "avax_time_lp",
+//     displayName: "TIME-AVAX LP",
+//     bondToken: "AVAX",
+//     bondIconSvg: AvaxTimeIcon,
+//     bondContractABI: LpBondContract,
+//     reserveContractAbi: LpReserveContract,
+//     networkAddrs: {
+//         [Networks.AVAX]: {
+//             bondAddress: "0xc26850686ce755FFb8690EA156E5A6cf03DcBDE1",
+//             reserveAddress: "0xf64e1c5B6E17031f5504481Ac8145F4c3eab4917",
+//         },
+//     },
+//     lpUrl: "https://www.traderjoexyz.com/#/pool/AVAX/0xb54f16fB19478766A268F172C9480f8da1a7c9C3",
+// });
 
 export default [mim, wavax, mimTime, avaxTime];
