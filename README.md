4009a213cc2693dca2e716ea4b32ea1319fd512b
appleseed-iii <appleseed-iii@protonmail.com>
2021-12-02 06:56:12 -0600

    view fixes & readme

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # todo
 
 - [ ] fix add token to wallet (need better svg)
+- [ ] update discord link to something permanent
+
+# contracts
+fetch = ohm
+bowl = bond contract for jlp, bond jlp for fetch
+kibble = the rewards contract = BondDepository
+lp = fetch-gohm joe pair = ohm-dai?
 
 # [FETCH Frontend](https://tbd.com/)
 This is the front-end repo for Fetch. 
--- src/views/Bond/BondHeader.tsx
+++ src/views/Bond/BondHeader.tsx
@@ -29,12 +29,12 @@ function BondHeader({ bond, slippage, onSlippageChange }: IBondHeaderProps) {
 
     useEscape(() => {
         if (open) handleClose;
-        else history.push("/mints");
+        else history.push("/bone");
     });
 
     return (
         <div className="bond-header">
-            <Link component={NavLink} to="/mints" className="cancel-bond">
+            <Link component={NavLink} to="/bone" className="cancel-bond">
                 <SvgIcon color="primary" component={XIcon} />
             </Link>
 
